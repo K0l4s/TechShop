ad452e029da0ec0bf0fb8c52fc81e123be625f85
MinhTri <ngminhtri2203@gmail.com>
2025-02-27 01:13:53 +0700

    UI CRUD Product

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -1,27 +1,29 @@
-import { Route, Routes } from 'react-router-dom'
-import LoginPage from '../pages/AuthPages/LoginPage'
-import AdminLayout from './layout/AdminLayout'
-import LandingPage from '../pages/MainPages/LandingPage/LandingPage'
-import MainLayout from './layout/MainLayout'
-import Dashboard from '../pages/AdminPages/DashboardPage/Dashboard'
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "../pages/AuthPages/LoginPage";
+import AdminLayout from "./layout/AdminLayout";
+import LandingPage from "../pages/MainPages/LandingPage/LandingPage";
+import MainLayout from "./layout/MainLayout";
+import Dashboard from "../pages/AdminPages/DashboardPage/Dashboard";
+import ProductPage from "../pages/AdminPages/ProductPage/ProductPage";
 
 const Router = () => {
   return (
     <Routes>
-        {/* Các path không cần chỉnh layout */}
-        <Route path="/login" element={<LoginPage />} />
-        {/* Các path cần chỉnh layout */}
-        {/* Admin */}
-        <Route path="/admin/*" element={<AdminLayout />} >
-            <Route path="" element={<Dashboard/>} />
-            <Route path="user" element={<div>Admin User</div>} />
-        </Route>
-        {/* User */}
-        <Route path="*" element={<MainLayout />} >
-            <Route path="" element={<LandingPage/>} />
-        </Route>
+      {/* Các path không cần chỉnh layout */}
+      <Route path="/login" element={<LoginPage />} />
+      {/* Các path cần chỉnh layout */}
+      {/* Admin */}
+      <Route path="/admin/*" element={<AdminLayout />}>
+        <Route path="products" element={<ProductPage />} />
+        <Route path="" element={<Dashboard />} />
+        <Route path="user" element={<div>Admin User</div>} />
+      </Route>
+      {/* User */}
+      <Route path="*" element={<MainLayout />}>
+        <Route path="" element={<LandingPage />} />
+      </Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
