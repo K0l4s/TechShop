bce23c0c04e28b84c9cfcc4554ce2dfce25e876c
Huỳnh Trung Kiên <21110223@student.hcmute.edu.vn>
2025-03-15 15:38:39 +0700

    add axios

---
+++ src/utils/axiosIntance.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import Cookie from "js-cookie";
+// import { BaseApi } from "./BaseApi";
+export const SERVER_PORT = import.meta.env.VITE_SERVER_PORT;
+export const SERVER_DOMAIN = import.meta.env.VITE_SERVER_DOMAIN;
+export const BaseApi = `http://${SERVER_DOMAIN}:${SERVER_PORT}`;
+
+// export const BaseApi = "https://destined-fly-rested.ngrok-free.app";
+// export const BaseApi = "https://525b-2402-800-63b6-96c5-801f-41b5-9d40-7623.ngrok-free.app"
+export const axiosInstance = axios.create({
+    baseURL: BaseApi,
+    headers: {
+        "Content-Type": "application/json",
+        "ngrok-skip-browser-warning": "69420"
+    },
+});
+export const axiosInstanceAuth = axios.create({
+    baseURL: BaseApi,
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + Cookie.get("token"),
+        "ngrok-skip-browser-warning": "69420"
+    },
+});
+
+export const axiosInstanceFile = axios.create({
+    baseURL: BaseApi,
+    withCredentials: true,
+    headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: "Bearer " + Cookie.get("token"),
+        "ngrok-skip-browser-warning": "69420"
+    },
+});
+
