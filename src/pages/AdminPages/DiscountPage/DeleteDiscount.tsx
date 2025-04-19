38f5f7a74fc23fca72a55bbc3a3c52e8a6ce884c
MinhTri <ngminhtri2203@gmail.com>
2025-04-20 00:45:37 +0700

    update discount

--- src/pages/AdminPages/DiscountPage/DeleteDiscount.tsx
+++ src/pages/AdminPages/DiscountPage/DeleteDiscount.tsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { DiscountService } from "../../../services/DiscountService";
 
 interface DeleteDiscountProps {
   handleClose: () => void;
-  handleDelete: () => void;
+  discountId: number; // ID của mã giảm giá
+  token: string; // Token xác thực
 }
 
 const DeleteDiscount: React.FC<DeleteDiscountProps> = ({
   handleClose,
-  handleDelete,
+  discountId,
+  token,
 }) => {
+  const handleDelete = async (id: number, token: string) => {
+    try {
+      await DiscountService.deleteDiscount(id, token);
+      // Call any action to update the UI or refetch data if needed
+      handleClose(); // Close the modal after successful delete
+    } catch (error) {
+      console.error("Lỗi khi xóa mã giảm giá:", error);
+    }
+  };
+
   return (
     <div className="fixed inset-0 bg-gray-300 bg-opacity-75 flex justify-center items-center">
       <div className="bg-[#ff6b6b] pt-8 pr-16 pb-8 pl-16 rounded-2xl shadow-lg w-auto relative text-white">
@@ -18,7 +31,7 @@ const DeleteDiscount: React.FC<DeleteDiscountProps> = ({
         </p>
         <div className="flex justify-center gap-4">
           <button
-            onClick={handleDelete}
+            onClick={() => handleDelete(discountId, token)} // Truyền token khi gọi hàm xóa
             className="bg-red-700 text-white font-bold px-4 py-2 rounded-lg hover:bg-red-800 transition duration-300"
           >
             Xóa
